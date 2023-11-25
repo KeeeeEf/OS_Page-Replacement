e458f9451dd0eff00bda9c38eccb00b02d15d30d
ysa <128650565+maelliecia@users.noreply.github.com>
2023-11-25 16:55:22 +0800

    Update opt.jsx

--- src/components/algos/opt.jsx
+++ src/components/algos/opt.jsx
@@ -52,9 +52,9 @@ function calculateReplacement(pages, nof){
             pageHits++;
 
             frameResults.push({
-                fames:[...frames],
+                frames:[...frames],
                 pageFault: false,
-                pageHIt: true,
+                pageHit: true,
             });
         }
     }
